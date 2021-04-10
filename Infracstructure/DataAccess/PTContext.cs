d3c162697f5a74547419822ca7d86f349f227a55
Ronal Gelvez <rmgelvez@hotmail.com>
2021-04-10 15:31:19 -0500

    Agregar archivos de proyecto.

---
+++ Infracstructure/DataAccess/PTContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infracstructure.DataAccess
+{
+    public class PTContext : DbContext
+    {
+        public PTContext(DbContextOptions<PTContext> options) : base(options)
+        {
+
+        }
+    }
+}
+
